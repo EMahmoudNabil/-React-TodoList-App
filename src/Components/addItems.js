0df05a490895201da2cdeffbb124d909c5217fd1
EMahmoudNabil <e.mahmoudnabil@gmail.com>
2020-06-06 18:35:39 +0200

    Create Folder Components

---
+++ src/Components/addItems.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import '../App.css';
+
+
+ class AddItems extends Component{
+     render(){
+        return(
+            <div>
+                 Add Items
+            </div>
+    
+        )
+    }
+    }
+
+
+
+
+export default AddItems;
---
+++ src/Components/todoItems.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import '../App.css';
+
+
+ function TodoItems(){
+    return(
+        <div>
+          Todo Lists
+        </div>
+  
+      )
+    }
+
+
+
+
+export default TodoItems;
