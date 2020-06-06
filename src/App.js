216cb8a7a20d537c6a99336449c7b9bb31db55bb
EMahmoudNabil <e.mahmoudnabil@gmail.com>
2020-06-06 20:32:03 +0200

    List Items Component

--- src/App.js
+++ src/App.js
@@ -8,10 +8,18 @@ import './App.css';
 
 
 class App extends Component{
+  state ={
+    items :[
+      {id:1 ,name:'mahmoud',age:22},
+      {id:2 ,name:'nabil',age:25},
+      {id:3 ,name:'rashad',age:26},
+  
+    ]
+  }
   render(){
     return(
      <div>
-     <TodoItems />
+     <TodoItems  items={this.state.items}/>
      <AddItems />
      </div>
       
--- src/Components/todoItems.js
+++ src/Components/todoItems.js
@@ -3,10 +3,26 @@ import React from 'react';
 import '../App.css';
 
 
- function TodoItems(){
+ const TodoItems = (props)=> {
+   const {items} = props;
+   const ListItems = items.map((item)=>{
+      return(
+        <div key={item.id}>
+          <span>{item.name}</span>
+          <span>{item.age}</span>
+          <span>&times;</span>
+
+        </div>
+      )
+   })
     return(
+        <div className="ListItem">
         <div>
-          Todo Lists
+          <span>Name</span>
+          <span>Age</span>
+          <span>Action</span>
+        </div>
+        {ListItems}
         </div>
   
       )
